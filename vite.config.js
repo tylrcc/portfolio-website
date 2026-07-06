5b8ab4551abbd8aaeaf7f18489bdf4ebfa696dab
tylrcc <247895347+tylrcc@users.noreply.github.com>
2026-07-06 14:27:42 -0400

    Fix unstyled production site: drop async-CSS inline onload blocked by CSP

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/** Let the inline boot shell paint before the full stylesheet downloads. */
-function asyncCssPlugin() {
-  return {
-    name: 'async-css',
-    transformIndexHtml: {
-      order: 'post',
-      handler(html) {
-        return html.replace(
-          /<link rel="stylesheet" crossorigin href="(\/assets\/[^"]+\.css)">/g,
-          '<link rel="preload" href="$1" as="style" crossorigin onload="this.onload=null;this.rel=\'stylesheet\'">\n    <noscript><link rel="stylesheet" crossorigin href="$1"></noscript>',
-        )
-      },
-    },
-  }
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), asyncCssPlugin()],
+  plugins: [react()],
   server: {
     fs: {
       strict: true,
